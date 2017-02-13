425ee202cc75da4bd8f940edc76843748ab2cdbb
t104368001 <t104368001@ntut.org.tw>
2017-02-13 15:57:53 +0800

    Camera for VS2013
    version 2
    讀取 WebCam
    灰階 WebCam

--- Camera/WebCam.cpp
+++ Camera/WebCam.cpp
@@ -1,14 +1,17 @@
-﻿/* 2016/10/18 14:26
-* WebCam version 01
-* 單純讀取 WebCam
+﻿/* 2016/10/26 14:24
+* WebCam version 02
+* 讀取 WebCam
+* 灰階 WebCam
 */
+
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 using namespace cv;
 
 int main()
 {
-	Mat image;
+	Mat image, gray_image;
 
 	VideoCapture cap;      //capture的宣告
 	cap.open(0);           //0 為預設camera
@@ -18,8 +21,9 @@ int main()
 		cap >> image;        //截取影像到image裡方法1
 		//cap.read(image); //截取影像到image裡方法2
 		//以上兩種方法都可以用，實測過沒問題!
-
-		imshow("Webcam live", image);
+		cvtColor(image, gray_image, CV_RGB2GRAY);  //截取影像轉成灰階影像
+		imshow("Webcam live", image);	//顯示原始即時影像
+		imshow("Webcam gray", gray_image);	//顯示灰階即時影像
 
 		waitKey(33);//避免CPU負荷，給點delay時間
 		//實際上一般webcam的framerate差不多33ms
